a156b60646395681b1fc89c592f626311a12f7c3
Jassim Ali <128520650+jassimali@users.noreply.github.com>
2023-10-08 12:39:03 +0530

    Update mars.js

--- facts/mars.js
+++ facts/mars.js
@@ -1,13 +1,12 @@
 const paragraphs = [
-    "Mars' surface has been altered by volcanoes, impacts, winds, and crustal movement",
-    "Phobos and Deimos are small compared to the planet",
+    "Mars surface has been altered by volcanoes, impacts, winds, and crustal movement",
+    "Phobos and Deimos are its moon which are small compared to the planet",
     "The first success was NASA's Mariner 4 flyby in 1965",
-    "Iron minerals in the Martian soil oxidize, or rust, causing the soil and atmosphere to look red"
-    
+    "Iron minerals in the Martian soil oxidize, or rust, causing the soil and atmosphere to look red",
+    "Mars has the largest volcano in the solar system",
+    "It has this atmosphere made up of CO2,N2 and Ar gases"
 ]; // Add your paragraph texts here
-
 let currentIndex = 0;
-
 function changeContent() {
    // document.getElementById("image").src = images[currentIndex];
     document.getElementById("paragraph").textContent = paragraphs[currentIndex];
@@ -15,7 +14,7 @@ function changeContent() {
 }
 
 // Call the changeContent function every 5 seconds
-setInterval(changeContent, 5000);
+setInterval(changeContent, 3000);
 
 // Initial call to display the first content
 changeContent();
