48510ee61230e4b34e11f3e7d44609de2ddde019
shraddhaskumar <126330717+shraddhaskumar@users.noreply.github.com>
2023-10-08 06:33:30 +0530

    Add files via upload

---
+++ nasa space apps/main7.js
@@ -0,0 +1,40 @@
+// script.js
+document.getElementById('check-answer').addEventListener('click', function() {
+  const selectedOption = document.querySelector('input[name="answer"]:checked');
+  const result = document.getElementById('result');
+
+  if (selectedOption) {
+      const userAnswer = selectedOption.value;
+      if (userAnswer === 'Venus Express') {
+          result.textContent = 'Correct!';
+          result.style.fontSize = '25px';
+          result.style.color = 'white';
+      } else {
+          result.textContent = 'Sorry, that\'s incorrect. The correct answer is Venus Express.';
+          result.style.color = 'white';
+          result.style.fontSize = '25px';
+      }
+  } else {
+      alert('Please select an answer.');
+  }
+});
+
+document.getElementById('NEXT').addEventListener('click', function() {
+  const next = document.querySelector('input[name="answer"]:checked');
+
+  if (next) {
+    //const next = selectedOption.value;
+    // if (userAnswer === 'Yuri Gagarin') {
+    //     result.textContent = 'Correct!';
+    //     result.style.fontSize = '25px';
+    //     result.style.color = 'white';
+    // } else {
+    //     result.textContent = 'Sorry, that\'s incorrect. The correct answer is Yuri Gagarin.';
+    //     result.style.color = 'white';
+    //     result.style.fontSize = '25px';
+    // }
+} else {
+    alert('Please select an answer.');
+}
+});
+
