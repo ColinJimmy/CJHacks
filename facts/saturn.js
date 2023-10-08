41dda259eef96a16e61a43f8a763cc2a3717abb2
Jassim Ali <128520650+jassimali@users.noreply.github.com>
2023-10-08 12:44:01 +0530

    Update saturn.js

--- facts/saturn.js
+++ facts/saturn.js
@@ -2,7 +2,9 @@ const paragraphs = [
     "A day on Saturn is only 10.7 hours and a year is 29 Earth years.",
     "Saturn is the least dense planet in the Solar System.",
     "Saturn has a moon bigger than Mercury.",
-    "Saturn has more moons than any other planet in the Solar System"
+    "Saturn has more moons than any other planet in the Solar System",
+    "Saturn Rings consist of Comets,Asteroids and shattered Moons",
+    "Saturns Magnetic field is smaller than jupiter"
 ]; // Add your paragraph texts here
 
 let currentIndex = 0;
@@ -14,7 +16,7 @@ function changeContent() {
 }
 
 // Call the changeContent function every 5 seconds
-setInterval(changeContent, 5000);
+setInterval(changeContent, 3000);
 
 // Initial call to display the first content
 changeContent();
