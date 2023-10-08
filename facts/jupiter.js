9a8b67d0a9ef8e793885217b21dc7b3726752d42
Jassim Ali <128520650+jassimali@users.noreply.github.com>
2023-10-08 12:41:46 +0530

    Update jupiter.js

--- facts/jupiter.js
+++ facts/jupiter.js
@@ -1,9 +1,11 @@
 const paragraphs = [
     "It takes Sunlight 43 minutes to travel from the Sun to Jupiter",
     "Jupiter has 80 moons",
-    "The composition of Jupiter is similar to that of the Sun mostly hydrogen and helium.",
+    "The composition of Jupiter Atmosphere is similar to that of the Sun mostly hydrogen and helium.",
     "There are clouds on Jupiter",
-    "A Spacecraft would have nowhere to land on Jupiter, it wouldnt be able to fly through unscathed either. The extreme pressures and temperatures deep inside the planet crush, melt, and vaporize spacecraft trying to fly into the planet."
+    "A Spacecraft would have nowhere to land on Jupiter, it wouldnt be able to fly through unscathed either. The extreme pressures and temperatures deep inside the planet crush, melt, and vaporize spacecraft trying to fly into the planet.",
+    "Jupiter has the largest moon , called Ganymede",
+    "Jupiter is known for its distinctive belts and zones"
 ]; // Add your paragraph texts here
 
 let currentIndex = 0;
@@ -15,7 +17,7 @@ function changeContent() {
 }
 
 // Call the changeContent function every 5 seconds
-setInterval(changeContent, 5000);
+setInterval(changeContent, 3000);
 
 // Initial call to display the first content
 changeContent();
