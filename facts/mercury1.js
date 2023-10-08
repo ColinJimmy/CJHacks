a40cd5e34e27c8ee92ef4432d73cd878932ab35f
Jassim Ali <128520650+jassimali@users.noreply.github.com>
2023-10-08 12:32:41 +0530

    Update mercury1.js

--- facts/mercury1.js
+++ facts/mercury1.js
@@ -3,7 +3,9 @@ const paragraphs = [
     "On Mercury a day is twice as long as a year",
     "Sunlight is 11 times brighter on Mercury",
     "A year on Mercury is 88 Earth days",
-    "Mercury orbits closest to the Sun."
+    "Mercury orbits closest to the Sun.",
+    "Second densest planet",
+    "Atmosphere consist of Oxygen,Sodium,Hydrogen,Helium etc"
 ]; // Add your paragraph texts here
 
 let currentIndex = 0;
@@ -14,8 +16,8 @@ function changeContent() {
     currentIndex = (currentIndex + 1) % paragraphs.length;
 }
 
-// Call the changeContent function every 5 seconds
-setInterval(changeContent, 5000);
+// Call the changeContent function every 3 seconds
+setInterval(changeContent, 3000);
 
 // Initial call to display the first content
 changeContent();
