85e31cb360da9cfea6244549d75b796f33994132
Jassim Ali <128520650+jassimali@users.noreply.github.com>
2023-10-08 12:45:44 +0530

    Update uranus.js

--- facts/uranus.js
+++ facts/uranus.js
@@ -3,7 +3,9 @@ const paragraphs = [
     "Uranus is the only planet not named after a Roman God",
     "The discoverer of Uranus wanted it to be called Georgium Sidus",
     "You could fit 63 Earths inside Uranus",
-    "It was the first planet to be discovered through a telescope"
+    "It was the first planet to be discovered through a telescope",
+    "Uranus ha 27 known Moons",
+    "It is one of 2 planet that rotates in opposite direction"
 ]; // Add your paragraph texts here
 
 let currentIndex = 0;
@@ -15,7 +17,7 @@ function changeContent() {
 }
 
 // Call the changeContent function every 5 seconds
-setInterval(changeContent, 5000);
+setInterval(changeContent, 3000);
 
 // Initial call to display the first content
 changeContent();
