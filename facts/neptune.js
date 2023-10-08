904ef94e7e7eadfad4ae07bd86db00b5cf0a01a4
Jassim Ali <128520650+jassimali@users.noreply.github.com>
2023-10-08 12:47:58 +0530

    Update neptune.js

--- facts/neptune.js
+++ facts/neptune.js
@@ -2,7 +2,9 @@ const paragraphs = [
     "Neptune is the densest of the giant planets",
     "Voyager 2 is the only spacecraft to visit Neptune",
     "Neptune has five rings and four more ring arcs",
-    "Neptune takes 165 Earth years to go around the Sun"
+    "Neptune takes 165 Earth years to go around the Sun",
+    "It has 14 Known moons",
+    "It doesnt have a solid surface"
 ]; // Add your paragraph texts here
 let currentIndex = 0;
 
@@ -13,7 +15,7 @@ function changeContent() {
 }
 
 // Call the changeContent function every 5 seconds
-setInterval(changeContent, 5000);
+setInterval(changeContent, 3000);
 
 // Initial call to display the first content
 changeContent();
